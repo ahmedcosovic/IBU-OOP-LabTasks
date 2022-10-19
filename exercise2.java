e902f14c3673c71fe5e4e9068ef823ac45a14746
ahmedcosovic <115624319+ahmedcosovic@users.noreply.github.com>
2022-10-19 22:02:52 +0200

    Delete exercise2.java

--- exercise2.java
+++
@@ -1,6 +0,0 @@
-public class exercise2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.err.println("(And all the people of the world)");
-    }
-}
