0db56e8feceb3f617f23ed126f7adcd8cbdcdf97
ahmedcosovic <115624319+ahmedcosovic@users.noreply.github.com>
2022-10-19 22:04:03 +0200

    Add files via upload

---
+++ week1/exercise2.java
@@ -0,0 +1,6 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.err.println("(And all the people of the world)");
+    }
+}
